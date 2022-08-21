16e6a12421951dd74c58d87d3dbb5136dafed237
Gabriel Mathias <gablemathias@icloud.com>
2022-08-21 14:30:51 -0300

    add rubocop default config

--- OOP-test-1/patient.rb
+++ OOP-test-1/patient.rb
@@ -1,5 +1,14 @@
-class Patient
+# Copyright (c) 2022 Gabriel Mathias "Gable" <gablemathias@icloud.com>
+#
+# Documentation by Gabriel Mathias
+#
+# All rights reserved. You can redistribute and/or modify it under the same terms as Ruby.
+#
+# Patient Class made for creating some patients to your amazing hospital or whatever
+#
+# LOOK this is just a comment practice at the top-level. Nothing more.
 
+class Patient
   def initialize(attributes={})
     @name = attributes[:name]
     @cured = attributes[:cured] || false
@@ -8,7 +17,6 @@ class Patient
   def cured
     @cured = true
   end
-
 end
 
 patient = Patient.new(name: 'John', cured: true)
--- OOP-test-1/room.rb
+++ OOP-test-1/room.rb
@@ -1,8 +1,7 @@
-require_relative "./patient.rb"
+require_relative './patient'
 
 class Room
-
-  def initialize(attributes={})
+  def initialize(attributes = {})
     @capacity = attributes[:capacity]
     @patients = attributes[:patients] || []
   end
@@ -11,22 +10,20 @@ class Room
     @patients << patient
   end
 
-  def full?
-    
-  end
+  # def full?
 
+  # end
 end
 
 room = Room.new(capacity: 2)
 
 p room
 
-sarah = Patient.new(name: "Sarah")
+sarah = Patient.new(name: 'Sarah')
 room.add(sarah)
 
-john = Patient.new(name: "John", cured: true)
+john = Patient.new(name: 'John', cured: true)
 
 room.add(john)
-
 
 p room
