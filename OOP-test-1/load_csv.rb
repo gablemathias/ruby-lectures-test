c5dc2c59496a24215956fffbb4dc4e4af6406dc5
Gabriel Mathias <gablemathias@icloud.com>
2022-08-21 19:15:09 -0300

    load_csv

--- OOP-test-1/load_csv.rb
+++ OOP-test-1/load_csv.rb
@@ -8,7 +8,9 @@ csv_file = "./patients.csv"
 CSV.foreach(csv_file, headers: :first_row, header_converters: :symbol) do |row|
   row[:id] = row[:id].to_i ## convert to integer
   row[:cured] = row[:cured] == 'true' ## convert to boolean
+  p row
   patients << Patient.new(row)
 end
 
-patients.each { |patient| p patient.name }
+# patients.each { |patient| p patient.name }
+# patients.each { |patient| p patient.cured }
