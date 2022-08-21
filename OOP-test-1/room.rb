19cbf52f907431051f579db5759ec26621dc7498
Gabriel Mathias <gablemathias@icloud.com>
2022-08-21 17:59:34 -0300

    add FullRoom error

--- OOP-test-1/room.rb
+++ OOP-test-1/room.rb
@@ -3,19 +3,23 @@ require_relative './patient'
 class Room
   attr_reader :patients
 
+  class FullRoom < StandardError; end
+
   def initialize(attributes = {})
     @capacity = attributes[:capacity]
     @patients = attributes[:patients] || []
   end
 
   def add(patient)
+    raise FullRoom, 'The room is full!' if full?
+
     patient.room = self
     @patients << patient
   end
 
-  # def full?
-  #   @capacity ==
-  # end
+  def full?
+    @capacity == @patients.length
+  end
 end
 
 room = Room.new(capacity: 2)
@@ -25,9 +29,21 @@ p room
 sarah = Patient.new(name: 'Sarah')
 room.add(sarah)
 
+puts 'Is the room full?'
+puts room.full?
+puts '---------'
+
 john = Patient.new(name: 'John', cured: true)
 
 room.add(john)
+
+puts 'Is the room full?'
+puts room.full?
+puts '---------'
+
+carlos = Patient.new(name: 'Carlos')
+
+room.add(carlos)
 
 room.patients.each do |patient|
   puts patient.name
@@ -42,3 +58,4 @@ room2 = Room.new(capacity: 3)
 room2.add(lucas)
 
 p room2
+  
